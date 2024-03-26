8a1cdd5d5db9460f2314d60f44a7660bbfdd691b
Lucas <storagemail329@gmail.com>
2024-03-26 12:31:51 -0300

    commit

---
+++ index.js
@@ -0,0 +1,44 @@
+function CalcularIndice() {
+let indice = 13;
+let k = 0;
+let soma = 0;
+
+while ( k < indice) {
+  k++;
+  soma += k;
+}
+
+console.log(soma);
+}
+
+// CalcularIndice();
+
+function Binet(valor) {
+  return (5*Math.pow(valor, 2) + 4);
+}
+
+function Fibonacci(valor) {
+   const resultado = Binet(valor);
+   if(Math.sqrt(resultado)) {
+    console.log("Pertence à sequência de Fibonacci");
+   } else {
+    console.log("Não pertence à sequência de Fibonacci");
+   }
+}
+
+// Fibonacci(101);
+
+
+function inverter(valor) {
+    let receberContador = [];
+    let inverso = [];
+   for(let i = valor.length; i > 0; i--) {
+      receberContador.push(i);
+   }
+   for(let i = 0; i < valor.length; i++) {
+      inverso.push(valor[receberContador[i]]);
+   }
+   console.log(inverso);
+}
+
+inverter("teste");
